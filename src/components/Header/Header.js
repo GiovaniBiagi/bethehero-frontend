dd8c93ea7edae14872b1b30b04748c3229f166a8
Giovani Alves <giovani.alves121@gmail.com>
2021-08-15 05:26:55 -0300

    First commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Header = ({children}) => (
+    <header>
+        <h1>{children}</h1>
+    </header>
+);
+
+export default Header;
